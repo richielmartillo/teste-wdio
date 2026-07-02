e601e2caed5ae3771253432fa7186176614f3720
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-07-02 20:21:30 -0300

    test: adiciona cenarios de login mobile com Page Object

---
+++ test/specs/login.spec.js
@@ -0,0 +1,32 @@
+import loginPage from "../pageobjects/login.page";
+
+describe('Funcionalidade: Login', () => {
+
+    beforeEach(async () => {
+        await driver.reloadSession()
+        await loginPage.abrirMenu()
+    })
+
+    it('Deve fazer login com sucesso', async () => {
+        await loginPage.preencherLogin('richard@teste.com', 'senha@123')
+
+        expect(await loginPage.mensagemAlerta()).toEqual('You are logged in!')
+
+        // await driver.acceptAlert()
+    });
+
+    it('Deve falhar ao fazer login com email inválido', async () => {
+        await loginPage.preencherLogin('teste@teste', 'senha@123')
+
+        await loginPage.mensagemErro('Please enter a valid email address')
+    });
+
+    it('Deve falhar ao fazer login com senha inválida', async () => {
+        //Please enter at least 8 characters
+        await loginPage.preencherLogin('teste@teste.com', '1234')
+
+        await loginPage.mensagemErro('Please enter at least 8 characters')
+
+    })
+
+});
